a9ea5948d80c4adccd3fb5a5880276733e8a3dcc
hukangkang <3051118483@qq.com>
2022-09-13 11:05:15 +0800

    主干添加2

--- src/com/threadt/Main.java
+++ src/com/threadt/Main.java
@@ -3,6 +3,7 @@ package com.threadt;
 public class Main {
     public static void main(String[] args) {
         System.out.println("分支dev");
-        System.out.println("主干添加");
+        System.out.println("主干添加1");
+        System.out.println("添加主干2");
     }
 }
